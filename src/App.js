685dc462f8901bdaf0737dae687030cf240e64c7
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-01-08 12:42:12 +0000

    bootstrap tested container and stack created with budget and expense buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Stack, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
 function App() {
-  return "nhagjag";
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
