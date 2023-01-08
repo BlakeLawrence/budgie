0fed41616ca539e2f15995220f9b4e7814f3fee6
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-01-08 12:55:36 +0000

    budget card created, custom utils function for gbp currency created and imported for use in budget card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Stack, Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
@@ -9,6 +10,16 @@ function App() {
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      </div>
     </Container>
   );
 }
